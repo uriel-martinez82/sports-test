61150015ab25d0d33258347c9e58e5b2ba53b126
Uriel Martinez <>
2026-04-23 20:56:18 -0300

    fix style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/sections/Hero";
 import SpaceTypes from "@/components/sections/SpaceTypes";
 import WhySportSolutions from "@/components/sections/WhySportSolutions";
 import Brands from "@/components/sections/Brands";
-import Stats from "@/components/sections/Metrics";
 import HomeCTA from "@/components/sections/HomeCTA";
 
 export default function HomePage() {
@@ -12,7 +11,6 @@ export default function HomePage() {
       <SpaceTypes />
       <WhySportSolutions />
       <Brands />
-      <Stats />
       <HomeCTA />
     </>
   );
